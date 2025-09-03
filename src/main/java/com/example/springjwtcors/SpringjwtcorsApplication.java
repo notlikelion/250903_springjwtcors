bfc6fd961d452c1c1cb0fc89ec7ad74a9ec90cf9
notlikelion <notlikelion@gmail.com>
2025-09-03 15:14:52 +0900

    Initial commit

---
+++ src/main/java/com/example/springjwtcors/SpringjwtcorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjwtcors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtcorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtcorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjwtcors/SpringjwtcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjwtcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjwtcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
