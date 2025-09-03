77ec393e42794570c9192d6177f27d7c0ffe1279
notlikelion <notlikelion@gmail.com>
2025-09-03 15:29:27 +0900

    ㅇ_ㅇ

---
+++ src/main/java/com/example/springjwtcors/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,37 @@
+package com.example.springjwtcors.filter;
+
+import com.example.springjwtcors.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // `Bearer {...}`
+        String token = resolveToken(request); // 요청 객체 -> 헤더 -> Authorization -> TOKEN
+
+        // Text가 null이거나 empty인지 (이러지 않으면) True
+        if (StringUtils.hasText(token) && jwtUtil.validateToken(token)) {
+            String username = jwtUtil.getUsernameFromToken(token);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // 이거가 맨 뒤에 잘 위치해야...
+        filterChain.doFilter(request, response);
+    }
+
+    private final JwtUtil jwtUtil;
+}
